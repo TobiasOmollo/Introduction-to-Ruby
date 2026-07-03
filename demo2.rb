e23272a9dba87ad28cc40e622ffa21c9ada7f8d9
TobiasOmollo <237821654+TobiasOmollo@users.noreply.github.com>
2026-07-03 10:50:52 +0300

    Indepth

--- demo2.rb
+++ demo2.rb
@@ -1,5 +1,23 @@
+#String interpolation
+m = "Sunderland FC"
+puts "#{m} has remained in the league"
+#For each loop
 strings = ["Apple", "Banana", "Orange", "Mango", "Grapes"]
 
 strings.each do |fruit|
   puts fruit
 end
+
+#While Loop
+x = 4
+names=[]
+
+while x>=0
+  puts "Enter name #{x}"
+  x -= 1
+  name=gets.chomp
+  names.push(name)
+end
+
+puts "\n "
+puts names
--- demo3.rb
+++ demo3.rb
@@ -3,3 +3,10 @@ items = ["Book", "Pen", "Laptop", "Phone", "Bag", "Shoes"]
 for i in 0...items.length
   puts "Item at index #{i}: #{items[i]}"
 end
+
+puts ("\n =")*20
+
+# Sample 2 for loop
+for a in 1..5 do
+  puts "items #{items[a]}"
+end
